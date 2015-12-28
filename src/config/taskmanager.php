4669f2d17a85f074b9c945fa13160ffbc015757d
areyin <fariz.studionexus@gmail.com>
2015-12-28 22:58:47 +0000

    Changed the configuration file

--- src/config/taskmanager.php
+++ src/config/taskmanager.php
@@ -7,8 +7,8 @@ return array(
      * Default formatting, list format and table format.
      * 
      */
-    'list_project_format' => '<li id="taskmanager:id"> :name (by :username) :delete_button </li>',
-    'list_tasks_format' => '<li id="taskmanager:id"> :name :done_task_button </li>',
+    'list_project_format' => '<li id="taskmanager_project:id"> :name (by :username) :delete_button </li>',
+    'list_tasks_format' => '<li id="taskmanager_task:id"> :name :complete_button </li>',
     
     /**
      * 
@@ -18,7 +18,7 @@ return array(
     'edit_url' => 'taskmanager/edit',
     'delete_project_url' => 'taskmanager/delete/project/',
     'delete_task_url' => 'taskmanager/delete/task/',
-    'done_url' => 'taskmanager/done',
+    'complete_url' => 'taskmanager/complete/task/',
 
     /**
      * 
@@ -27,7 +27,7 @@ return array(
      */
     'view_button' => '<a href=":view_url/:id" class="btn btn-success">View</a>',  
     'edit_button' => '<a href=":edit_url/:id" class="btn btn-primary">Edit</a>',  
-    'delete_button' => '<a href="#" onclick="changeProject(:id, \':delete_url\'); return false" class="btn btn-danger">Delete</a>',  
-    'done_task_button' => '<a href=":done_url/:id" class="btn btn-success">Done</a>', 
+    'delete_button' => '<a href="#" onclick="change(\':type\', :id, \':delete_url\'); return false" class="btn btn-danger">Delete</a>',  
+    'complete_button' => '<a href="#" onclick="change(\':type\', :id, \':complete_url\'); return false" class="btn btn-success">Complete</a>', 
     
 );
